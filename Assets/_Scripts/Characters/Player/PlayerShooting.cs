30003d12d7780cfa55f0f84f98bfafa6dfe0c17f
spiderface721 <borys2323@onet.pl>
2025-08-02 13:09:16 +0200

    player shooting

--- Assets/_Scripts/Characters/Player/PlayerShooting.cs
+++ Assets/_Scripts/Characters/Player/PlayerShooting.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Net.Sockets;
 using Unity.Mathematics;
 using UnityEngine;
@@ -8,6 +9,9 @@ public class PlayerShooting : MonoBehaviour
     [SerializeField]Transform bulletSpawnPoint;
     [SerializeField]GameObject bulletPrefab;
     public float BulletSpeed = 10f;
+    public float ShootingSpeed = 0.25f;
+
+    float cooldownEndTimeInSeconds;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,12 +22,23 @@ public class PlayerShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Mouse.current.leftButton.isPressed) Attack();
     }
+    
 
     public void Attack()
     {
-        Shoot();
+        //if not on cooldown perform shoot() and add shootingspeed to cooldown
+        if (cooldownEndTimeInSeconds < Time.time) 
+        {
+            Shoot();
+            AddCoolDownInSeconds(ShootingSpeed);
+        }
+    }
+
+    public void AddCoolDownInSeconds(float addedTime)
+    {
+        cooldownEndTimeInSeconds = Time.time + addedTime;
     }
 
     void Shoot()
