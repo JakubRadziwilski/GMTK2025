51433fb71673d4fff84c378dec3a4432081675a8
spiderface721 <borys2323@onet.pl>
2025-08-02 20:24:08 +0200

    pointshop

--- Assets/_Scripts/Characters/PlayerStats.cs
+++ Assets/_Scripts/Characters/PlayerStats.cs
@@ -4,7 +4,7 @@ using System;
 public class PlayerStats : MonoBehaviour
 {
     public static PlayerStats Instance { get; private set; } // Singleton instance of PlayerStats
-    
+
     private void Awake()
     {
         if (Instance == null)
@@ -20,10 +20,14 @@ public class PlayerStats : MonoBehaviour
     bool isRunning = false; // Flag to check if the player is currently running
 
     public Transform player; // Reference to the player's transform
+
+    public int points = 0;  //Points to spend in the shop 
+    public int scoreEarned = 0; // Points earned in the whole game, does not count spending
     public int maxHealth = 100; // Maximum health of the player
     public float speed = 1000f; // Speed of the player
     public float shootingSpeed = 1f; // Shooting speed of the player
     public int damage = 10; // Damage dealt by the player
+
 
     public event Action onRunEnd; // Event to notify when player stats change
 
@@ -42,11 +46,21 @@ public class PlayerStats : MonoBehaviour
 
     public void EndCurrentRun()
     {
-        if(isRunning)
+        if (isRunning)
         {
             isRunning = false; // Set the running flag to false
             onRunEnd?.Invoke(); // Invoke the event when the run ends
             Debug.Log("Ending current run.");
         }
+    }
+
+    public void AddPoints(int amount)
+    {
+        points += amount;
+        scoreEarned += amount;
+    }
+    public void SubtractPoints(int amount)
+    {
+        points -= amount;
     }
 }
